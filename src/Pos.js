1a9dc82498bb77cf0a6a2fbef2e694a9b41f721c
ilyamelnik <43278944+Ilyamelnik@users.noreply.github.com>
2023-07-16 14:49:45 -0300

    Update Pos.js

--- src/Pos.js
+++ src/Pos.js
@@ -1,12 +1,17 @@
 import DesktopHeader from "./DesktopHeader";
 import React, {useState} from 'react';
 import SearchDropHeaderNothidden from "./SearchDropHeaderNothidden";
+
+//Что такое pos, что это значит я как друогой разработчик не понимаю
+//неотфарматирован файл, некрасиво
 const Pos = () => {
+
     const [isShowDropHeader, setIsShowDropHeader] = useState(false);
+
     const sayHi = () =>{
-     setIsShowDropHeader (!isShowDropHeader)
-      
+     setIsShowDropHeader (!isShowDropHeader)      
   }
+
     return (
       <div className = "pos" >
         <DesktopHeader showDropHeader={sayHi} />
